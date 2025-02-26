e64a31b63537d724b73e9bff55d55bea099a28ce
ikemal12 <iltar2005@gmail.com>
2025-02-26 20:33:41 +0000

    Added jump and load instructions

--- src/chip.cpp
+++ src/chip.cpp
@@ -1,5 +1,6 @@
 #include "chip.hpp"
 #include <cstdint>
+#include <cstring>
 #include <fstream>
 #include <chrono>
 #include <random>
@@ -27,20 +28,6 @@ uint8_t fontset[FONTSET_SIZE] = {
 	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-class Chip8 {
-    public:
-    uint8_t registers[16]{};
-    uint8_t memory[4096]{};
-    uint16_t index{};
-    uint16_t pc{};
-    uint16_t stack[16]{};
-    uint8_t sp{};
-    uint8_t delayTimer{};
-    uint8_t soundTimer{};
-    uint8_t keypad[16]{};
-    uint32_t video[64 * 32]{};
-    uint16_t opcode;
-};
 
 void Chip8::LoadROM(char const* filename) {
     std::ifstream file(filename, std::ios::binary | std::ios::ate);
@@ -53,7 +40,7 @@ void Chip8::LoadROM(char const* filename) {
         file.read(buffer, size);
         file.close();
 
-        for (long i=0; i<size, ++i) {
+        for (long i=0; i<size; ++i) {
             memory[START_ADDRESS + i] = buffer[i];
         }
         delete[] buffer;
@@ -72,7 +59,137 @@ Chip8::Chip8()
 
     randByte = std::uniform_int_distribution<uint8_t>(0,255U);
 
-    
+
 }
 
+void Chip8::OP_00E0() {
+    memset(video, 0, sizeof(video));
+}
 
+void Chip8::OP_00EE() {
+    --sp;
+    pc = stack[sp];
+}
+
+void Chip8::OP_1nnn() {
+    uint16_t address = opcode & 0x0FFFu;
+    pc = address;
+}
+
+void Chip8::OP_2nnn() {
+    uint16_t address = opcode & 0x0FFFu;
+    stack[sp] = pc;
+    ++sp;
+    pc = address;
+}
+
+void Chip8::OP_3xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    if (registers[Vx] == byte) {
+        pc += 2;
+    }
+}
+
+void Chip8::OP_4xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    if (registers[Vx] != byte) {
+        pc += 2;
+    }
+}
+
+void Chip8::OP_5xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] == registers[Vy]) {
+
+    }
+}
+
+void Chip8::OP_6xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] = byte;
+}
+
+void Chip8::OP_7xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] += byte;
+}
+
+void Chip8::OP_8xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] = registers[Vy];
+}
+
+void Chip8::OP_8xy1() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] |= registers[Vy];
+}
+
+void Chip8::OP_8xy2() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] &= registers[Vy];
+}
+
+void Chip8::OP_8xy3() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] ^= registers[Vy];
+}
+
+void Chip8::OP_8xy4() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    uint16_t sum = registers[Vx] + registers[Vy];
+
+    if (sum > 255U) {
+        registers[0xF] = 1;
+    } else {
+        registers[0xF] = 0;
+    }
+    registers[Vx] = sum & 0xFFu;
+}
+
+void Chip8::OP_8xy5() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] > registers[Vy]) {
+        registers[0xF] = 1;
+    } else {
+        registers[0xF] = 0;
+    }
+    registers[Vx] -= registers[Vy];
+}
+
+void Chip8::OP_8xy6() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    registers[0xF] = (registers[Vx] & 0x1u);
+
+    registers[Vx] >>= 1;
+}
+
+void Chip8::OP_8xy6() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    registers[0xF] = (registers[Vx] & 0x1u);
+
+    registers[Vx] >>= 1;
+}
--- src/chip.hpp
+++ src/chip.hpp
@@ -1,6 +1,22 @@
+#pragma once
+
+#include <random>
+
 class Chip8 {
 
     public:
+        uint8_t registers[16]{};
+        uint8_t memory[4096]{};
+        uint16_t index{};
+        uint16_t pc{};
+        uint16_t stack[16]{};
+        uint8_t sp{};
+        uint8_t delayTimer{};
+        uint8_t soundTimer{};
+        uint8_t keypad[16]{};
+        uint32_t video[64 * 32]{};
+        uint16_t opcode;
+        
         Chip8();
         void LoadROM(char const* filename);
 
@@ -9,10 +25,23 @@ class Chip8 {
         std::default_random_engine randGen;
 	    std::uniform_int_distribution<uint8_t> randByte;
 
-
+        void OP_00E0();
+        void OP_00EE();
+        void OP_1nnn();
+        void OP_2nnn();
+        void OP_3xkk();
+        void OP_4xkk();
+        void OP_5xy0();
+        void OP_6xkk();
+        void OP_7xkk();
+        void OP_8xy0();
+        void OP_8xy1();
+        void OP_8xy2();
+        void OP_8xy3();
+        void OP_8xy4();
+        void OP_8xy5();
+        void OP_8xy6();
 
 
 };
     
-
-
