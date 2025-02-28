1238f64cd5bcc172fbb661f8393fd521722af217
ikemal12 <iltar2005@gmail.com>
2025-02-28 15:38:48 +0000

    Setup framebuffer texture and update rendering logic

--- src/platform.cpp
+++ src/platform.cpp
@@ -20,10 +20,28 @@ Platform::Platform(char const* title, int windowWidth, int windowHeight, int tex
     gl_context = SDL_GL_CreateContext(window);
     SDL_GL_SetSwapInterval(1);
     gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+
+    glGenTextures(1, &framebuffer_texture);
+    glBindTexture(GL_TEXTURE_2D, framebuffer_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 640, 320, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glBindTexture(GL_TEXTURE_2D, 0);
     
 }
 
 Platform::~Platform() {
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+void Platform::Update(void const* buffer, int pitch) {
+    SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
 }
--- src/platform.hpp
+++ src/platform.hpp
@@ -8,10 +8,12 @@ class Platform {
     public:
         Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
         ~Platform();
+        void Update(void const* buffer, int pitch);
 
     private:
         SDL_Window* window{};
         SDL_Renderer* renderer{};
         SDL_Texture* texture{};
         SDL_GLContext gl_context{};
+        GLuint framebuffer_texture;
 }
